9243a9bf635752e40a3be941644b5ef534bb6fff
rabeekiblawi <rabeekiblawi@gmail.com>
2018-01-10 09:25:36 +0200

    GA Unity

---
+++ CSharpPlayGround.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CSharpPlayGround : MonoBehaviour {
+
+    List<char> things;
+    List<char> other_things;
+    private void Start()
+    {
+
+        things = new List<char>();
+        other_things = new List<char>();
+        populateList();
+        foreach (var item in things)
+        {
+            other_things.Add(item);
+        }
+        things.Clear();
+        print(other_things[0]);
+
+    }
+
+
+
+    void populateList() {
+        things.Add('a');
+        things.Add('b');
+        things.Add('c');
+    }
+}
